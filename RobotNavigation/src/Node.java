d18b4210662f575d533ac9b57babe726633a5e8d
ZakiNata <zaki.natsheh@gmail.com>
2017-07-21 03:20:29 +0300

    Creating the project
    first push

---
+++ RobotNavigation/src/Node.java
@@ -0,0 +1,15 @@
+
+public class Node {
+
+		private NodeStatus Status;
+		
+		public Node(NodeStatus status) {
+			Status = status;
+		}
+		public NodeStatus getStatus() {
+			return Status;
+		}
+		public void setStatus(NodeStatus status) {
+			Status = status;
+		} 
+}
---
+++ RobotNavigation/src/NodeStatus.java
@@ -0,0 +1,24 @@
+
+public enum NodeStatus {
+	  Start {
+	      public String toString() {
+	          return "Start";
+	      }
+	  },
+
+	  Goal {
+	      public String toString() {
+	          return "Goal";
+	      }
+	  },	
+	  Restricted {
+	      public String toString() {
+	          return "Restricted Node";
+	      }
+	  },
+	  Allowed {
+	      public String toString() {
+	          return "Allowed Node";
+	      }
+	  }	 
+}
